a8cfe9042a0eb1dbb2d4f05b2f2fa6f176592a4e
Anusree <anilkumaranusree113@gmail.com>
2024-12-13 12:39:13 +0530

    updated header

---
+++ src/components/header/Header.js
@@ -0,0 +1,50 @@
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import React from "react";
+import { ShoppingCart } from "@mui/icons-material";
+import SearchBar from "./SearchBar";
+
+export default function Header({ setFilters }) {
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+      }}
+    >
+      <AppBar
+        position="static"
+        color="transparent"
+        sx={{ boxShadow: "none", borderBottom: "1px solid grey" }}
+      >
+        <Toolbar sx={{ diplay: "flex", justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2, display: "flex", alignItems: "center" }}
+          >
+            <img src="/logo.png" alt="logo" width={50} />
+            <Box
+              sx={{
+                display: { xs: "none", sm: "none", md: "flex" },
+                alignItems: "center",
+              }}
+            >
+              <img src="/logo2.png" alt="logo" />
+            </Box>
+          </IconButton>
+          <SearchBar setFilters={setFilters} />
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <ShoppingCart />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/components/header/SearchBar.js
@@ -0,0 +1,84 @@
+import {  InputBase, styled } from "@mui/material";
+import React, { useState, useCallback } from "react";
+import { Search } from "@mui/icons-material";
+
+const SearchBox = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  borderRadius: theme.shape.borderRadius,
+  marginRight: 0,
+  width: "80%",
+  height: "40px",
+  border: "1px solid black",
+  [theme.breakpoints.up("xs")]: {
+    marginRight: theme.spacing(1),
+    width: "50%",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  background: "#30a106",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "80vw",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 2, 1, 2),
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "40vw",
+      "&:focus": {
+        width: "40vw",
+      },
+    },
+  },
+}));
+
+const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
+
+export default function SearchBar({ setFilters }) {
+  const [input, setInput] = useState("");
+
+  const handleSearch = () => {
+    setFilters((prev) => {
+      return { ...prev, searchInput: input.toLowerCase() };
+    });
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedInput = useCallback(
+    debounce((value) => setInput(value), 100),
+    []
+  );
+
+  const handleInput = (e) => {
+    debouncedInput(e.target.value);
+  };
+
+  console.log(input);
+
+  return (
+    <SearchBox>
+      <StyledInputBase
+        placeholder="Search for Products…"
+        inputProps={{ "aria-label": "search" }}
+        onChange={handleInput}
+      />
+      <SearchIconWrapper onClick={handleSearch}>
+        <Search sx={{ color: "white" }} />
+      </SearchIconWrapper>
+    </SearchBox>
+  );
+}
